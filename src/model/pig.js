7d7a041582423ae95ab226acef69a1ac1d8a4312
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-04-05 12:04:21 +0700

    add transformation boundary for pig

--- src/model/pig.js
+++ src/model/pig.js
@@ -24,12 +24,29 @@ class MinecraftPigModel extends GeoObject {
         this.parts["nose"].applyTransformation()
         this.parts["nose"].id = "nose"
         this.parts["nose"].mid = [0, 0.05, -0.7]
+        this.parts["nose"].bound = {
+            translate: {
+                activation: [true, true, false],
+                range: [[-0.02, 0.02, 0.001], [-0.02, 0.02, 0.001], []]
+            },
+            rotate: {
+                activation: [true, false, false],
+                range: [[-10, 10, 1], [], []]
+            }
+        }
 
         this.parts["head"].addScaling3D([0.4, 0.4, 0.4])
         this.parts["head"].addTranslation([0, 0.1, -0.5])
         this.parts["head"].applyTransformation()
         this.parts["head"].id = "head"
         this.parts["head"].mid = [0, 0.1, -0.3]
+        this.parts["head"].bound = {
+            ...this.parts["head"].bound,
+            rotate: {
+                activation: [true, false, false],
+                range: [[-90, 90, 1], [], []]
+            }
+        }
 
         const keys = ["right-arm", "left-arm", "right-leg", "left-leg"]
         keys.forEach((k, i) => {
@@ -42,11 +59,28 @@ class MinecraftPigModel extends GeoObject {
             this.parts[k].applyTransformation()
             this.parts[k].id = k
             this.parts[k].mid = [0, -0.2, z]
+            this.parts[k].bound = {
+                ...this.parts[k].bound,
+                rotate: {
+                    activation: [true, false, false],
+                    range: [[-90, 90, 1], [], []]
+                }
+            }    
         })
 
         this.parts["body"].addScaling3D([0.5, 0.4, 0.8])
         this.parts["body"].applyTransformation()
         this.parts["body"].id = "body"
+        this.parts["body"].bound = {
+            translate: {
+                activation: [true, true, true],
+                range: [[-5, 5, 0.01], [-5, 5, 0.01], [-5, 5, 0.01]]
+            },
+            rotate: {
+                activation: [true, true, true],
+                range: [[-180, 180, 1], [-180, 180, 1], [-180, 180, 1]]
+            }
+        }
 
         this.parts["body"].addChild(this.parts["head"])
         this.parts["body"].addChild(this.parts["right-arm"])
